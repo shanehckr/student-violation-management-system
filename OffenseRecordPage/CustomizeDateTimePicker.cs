0303a4caa2cf5ac75a61875cd9ba2098e80603d5
Shane <kearldelapaz@gmail.com>
2025-11-29 21:14:29 +0800

    Add Admin View Pages

---
+++ OffenseRecordPage/CustomizeDateTimePicker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffenseRecordPage
+{
+    internal class CustomizeDateTimePicker : DateTimePicker
+
+    {
+        public CustomizeDateTimePicker()
+        {
+            this.Height = 40;
+        }
+
+    }
+}
